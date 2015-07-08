1ee97cc9012ea039fcd489a7ddb891d44e702f41
miskun <miska.kaipiainen@gmail.com>
2015-07-08 19:37:31 +0300

    fixing case sensitive issue with README.md files

--- index.js
+++ index.js
@@ -33,7 +33,6 @@ var expressgh = function(root, options){
     o.syncUrl = o.syncUrl || "_sync";
     o.syncOnStart = o.syncOnStart || false;
     o.root = path.resolve(root);
-    o.debug = o.debug || false;
 
     // sanitize sync url; ensure leading "/" and remove trailing "/"
     if((o.syncUrl.length > 0) && (o.syncUrl[0] != "/")) o.syncUrl = "/" + o.syncUrl;
@@ -61,18 +60,12 @@ var expressgh = function(root, options){
         // remove trailing slash
         if(filePath[filePath.length-1] === path.sep) filePath = filePath.slice(0,-1);
 
-        if(o.debug) console.log("Resolving filePath: " + filePath);
-
         // is sync url?
-        if(o.debug) console.log("Is it sync url?");
         if(req.url == o.syncUrl){
-            if(o.debug) console.log("Yes! Sync now!");
             syncGithub(o, function(e){
                 if(!e){
-                    if(o.debug) console.log("Sync with GitHub repository ["+ o.ghUser + "/" + o.ghRepo + "] done!");
                     res.send("Sync with GitHub repository ["+ o.ghUser + "/" + o.ghRepo + "] done!");
                 } else {
-                    if(o.debug) console.log("Unable to sync with GitHub repository ["+ o.ghUser + "/" + o.ghRepo + "]!");
                     res.send("Unable to sync with GitHub repository ["+ o.ghUser + "/" + o.ghRepo + "]!");
                 }
             });
@@ -80,39 +73,28 @@ var expressgh = function(root, options){
         }
 
         // lookup for the .md file, else serve static file
-        if(o.debug) console.log("Is there corresponding .md file ["+filePath + ".md"+"] or ["+filePath + path.sep + "readme.md"+"]?");
         if(fs.existsSync(filePath + ".md")){
 
             filePath = filePath + ".md";
-            if(o.debug) console.log("Yes! Found .md file: " + filePath);
 
-        } else if(fs.existsSync(filePath + path.sep + "readme.md")){
+        } else if(fs.existsSync(filePath + path.sep + "README.md")){
 
             // just to preserve relative paths in .md files
-
-            if(o.debug) console.log("Need for Redirect?");
-
             if(req.originalUrl[req.originalUrl.length-1] !== "/" ){
 
-                if(o.debug) console.log("Yes, actually we need to redirect to " + req.originalUrl + "/");
                 res.redirect(req.originalUrl + "/");
                 return;
 
             }
 
-            filePath = filePath + path.sep + "readme.md";
-            if(o.debug) console.log("No need for redirect. Got .md file: " + filePath);
+            filePath = filePath + path.sep + "README.md";
 
         } else {
-
-            if(o.debug) console.log("Alright, no .md files. Trying to get static assets...");
 
             serveStatic(req, res, next);
             return;
 
         }
-
-        if(o.debug) console.log("Loading file for processing..." + filePath);
 
         // get the file contents
         var rawFile = fs.readFileSync(filePath, 'utf8');
@@ -126,11 +108,7 @@ var expressgh = function(root, options){
             ghDir: o.ghDir
         });
 
-        if(o.debug) console.log("Parsing file...");
-        if(o.debug) console.log(req.egd);
-
         // get table of contents
-        if(o.debug) console.log("Fetching TOC...");
         var toc = new Toc(o.root);
         toc.get(function(tocErr, tocRes){
 
@@ -141,7 +119,6 @@ var expressgh = function(root, options){
             // add table of contents
             attr.egdtoc = "";
             if(!tocErr){
-                if(o.debug) console.log("Parsing TOC...");
                 attr.egdtoc = tocRes.toHTML({
                     rebasePath: req.baseUrl,
                     selected: req.url
@@ -151,8 +128,6 @@ var expressgh = function(root, options){
             // render
             if(o.render && req.egd && (typeof req.egd.content !== 'undefined')){
 
-                if(o.debug) console.log("Rendering...");
-
                 // if no layout or template, use defaults
                 if(!attr.layout) attr.layout = o.defaultLayout;
                 if(!attr.template) attr.template = o.defaultTemplate;
@@ -160,9 +135,6 @@ var expressgh = function(root, options){
                 res.render(attr.template, attr);
 
             } else {
-
-                if(o.debug) console.log("Nothing to Render!!!! Something is WRONG!!!");
-                if(o.debug) console.log(o.render, req.egd, (typeof req.egd.content !== 'undefined'));
 
                 next();
 
